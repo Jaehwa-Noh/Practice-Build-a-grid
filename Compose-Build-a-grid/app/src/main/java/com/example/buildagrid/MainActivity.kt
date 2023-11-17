29a5d83adf330ec6f108a3575ca3e60b7f6b78d3
Jaehwa Noh <shwoghk14@gmail.com>
2023-11-17 21:17:27 +0900

    Create TopicCard composable.

--- Compose-Build-a-grid/app/src/main/java/com/example/buildagrid/MainActivity.kt
+++ Compose-Build-a-grid/app/src/main/java/com/example/buildagrid/MainActivity.kt
@@ -3,17 +3,26 @@ package com.example.buildagrid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.buildagrid.data.DataSource
+import com.example.buildagrid.model.Topic
 import com.example.buildagrid.ui.theme.BuildAGridTheme
 
 class MainActivity : ComponentActivity() {
@@ -46,7 +55,41 @@ fun CourseGrid(modifier: Modifier = Modifier) {
         modifier = modifier
     ) {
         items(topicLists.size) { index ->
-            Text(text = stringResource(id = topicLists[index].stringResourceId))
+            TopicCard(topic = topicLists[index])
+        }
+    }
+}
+
+@Composable
+fun TopicCard(
+    topic: Topic,
+    modifier: Modifier = Modifier
+) {
+    Card(modifier = modifier) {
+        Row {
+            Image(
+                painter = painterResource(topic.drawableResourceId),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(68.dp)
+            )
+            Column(
+                modifier = Modifier
+                    .padding(start = 16.dp, top = 16.dp, end = 16.dp)
+            ) {
+                Text(
+                    text = stringResource(id = topic.stringResourceId),
+                    modifier = Modifier.padding(bottom = 8.dp)
+                )
+                Row {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_grain),
+                        contentDescription = null,
+                        modifier = Modifier.padding(end = 8.dp)
+                    )
+                    Text(text = topic.courseNumber.toString())
+                }
+            }
         }
     }
 }
