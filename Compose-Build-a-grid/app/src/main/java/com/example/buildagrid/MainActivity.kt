01f9540041996c1d40f1318d1e74cd42f4c1b6f1
Jaehwa Noh <shwoghk14@gmail.com>
2023-11-17 18:16:34 +0900

    Create CourseGrid composable.

--- Compose-Build-a-grid/app/src/main/java/com/example/buildagrid/MainActivity.kt
+++ Compose-Build-a-grid/app/src/main/java/com/example/buildagrid/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.buildagrid.data.DataSource
 import com.example.buildagrid.ui.theme.BuildAGridTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +35,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CourseApp(modifier: Modifier = Modifier) {
+    CourseGrid(modifier)
+}
 
+@Composable
+fun CourseGrid(modifier: Modifier = Modifier) {
+    val topicLists = DataSource.topics
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        modifier = modifier
+    ) {
+        items(topicLists.size) { index ->
+            Text(text = stringResource(id = topicLists[index].stringResourceId))
+        }
+    }
 }
 
 @Preview(showBackground = true)
