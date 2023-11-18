f4279a37625d2eda2d91428a2d9fd95732a6ea7f
shwoghk14 <shwoghk14@gmail.com>
2023-11-18 20:15:44 +0900

    Create Topic struct.

---
+++ SwiftUI-Build-a-Grid/Build a Grid/Model/Topic.swift
@@ -0,0 +1,12 @@
+//
+//  Topic.swift
+//  Build a Grid
+//
+//  Created by Jaehwa Noh on 11/18/23.
+//
+
+struct Topic {
+    let stringResource: String
+    let courseNumber: Int
+    let drawableResourceName: String
+}
