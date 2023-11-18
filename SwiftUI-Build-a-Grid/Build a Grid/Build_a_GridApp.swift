ace12b54cdc03af07cc34328f590564a93d344ef
shwoghk14 <shwoghk14@gmail.com>
2023-11-18 20:09:24 +0900

    Init project.

---
+++ SwiftUI-Build-a-Grid/Build a Grid/Build_a_GridApp.swift
@@ -0,0 +1,17 @@
+//
+//  Build_a_GridApp.swift
+//  Build a Grid
+//
+//  Created by Jaehwa Noh on 11/18/23.
+//
+
+import SwiftUI
+
+@main
+struct Build_a_GridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
