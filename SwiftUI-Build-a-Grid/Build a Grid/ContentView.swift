73acf5e686b49334d349f62d3cf817037318522d
shwoghk14 <shwoghk14@gmail.com>
2023-11-18 23:16:33 +0900

    Create CardView.

--- SwiftUI-Build-a-Grid/Build a Grid/ContentView.swift
+++ SwiftUI-Build-a-Grid/Build a Grid/ContentView.swift
@@ -19,9 +19,41 @@ struct GridView: View {
     var body: some View {
         LazyVGrid(columns: [GridItem(), GridItem()]) {
             ForEach(topics) { topic in
-                Text(topic.stringResource)
+                CardView(topic: topic)
             }
         }
+    }
+}
+
+struct CardView: View {
+    let topic: Topic
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            Image(topic.drawableResourceName)
+                .resizable()
+                .frame(width: 68, height: 68)
+                .padding(.trailing, 16)
+            VStack(alignment: .leading, spacing: 0) {
+                
+                Text(topic.stringResource)
+                    .padding([.top, .trailing], 16)
+                    .padding(.bottom, 8)
+                    .font(.body)
+                    .frame(alignment: .center)
+                
+                HStack(spacing: 0) {
+                    Image("ic_grain")
+                        .resizable()
+                        .frame(width: 24, height: 24)
+                        .padding(.trailing, 16)
+                    Text("\(topic.courseNumber)")
+                        .font(.footnote)
+                    Spacer()
+                }
+            }
+        }
+        .background(Color("CardBackground"))
     }
 }
 
